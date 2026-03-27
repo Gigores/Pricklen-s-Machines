48ea353ec039406cdc53cb27e69addf4720fa509
Pricklen <igorsamodumskij@gmail.com>
2026-03-27 23:28:13 +0300

    better jei compat

--- src/main/java/com/pricklen/machines/compat/KilnCategory.java
+++ src/main/java/com/pricklen/machines/compat/KilnCategory.java
@@ -6,11 +6,14 @@ import com.pricklen.machines.recipe.KilnRecipe;
 import mezz.jei.api.constants.VanillaTypes;
 import mezz.jei.api.gui.builder.IRecipeLayoutBuilder;
 import mezz.jei.api.gui.drawable.IDrawable;
+import mezz.jei.api.gui.drawable.IDrawableAnimated;
+import mezz.jei.api.gui.ingredient.IRecipeSlotsView;
 import mezz.jei.api.helpers.IGuiHelper;
 import mezz.jei.api.recipe.IFocusGroup;
 import mezz.jei.api.recipe.RecipeIngredientRole;
 import mezz.jei.api.recipe.RecipeType;
 import mezz.jei.api.recipe.category.IRecipeCategory;
+import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.ItemStack;
@@ -28,10 +31,26 @@ public class KilnCategory implements IRecipeCategory<KilnRecipe> {
 
     private final IDrawable background;
     private final IDrawable icon;
+    private final IDrawableAnimated flame;
+    private final IDrawableAnimated arrow;
 
     public KilnCategory(IGuiHelper helper) {
         this.background = helper.createDrawable(TEXTURE, 52, 13, 88, 60);
         this.icon = helper.createDrawableIngredient(VanillaTypes.ITEM_STACK, new ItemStack(ModBlocks.KILN.get()));
+
+        this.flame = helper.createAnimatedDrawable(
+                helper.createDrawable(TEXTURE, 176, 0, 14, 14),
+                200,
+                IDrawableAnimated.StartDirection.TOP,
+                true
+        );
+
+        this.arrow = helper.createAnimatedDrawable(
+                helper.createDrawable(TEXTURE, 176, 14, 24, 17),
+                200,
+                IDrawableAnimated.StartDirection.LEFT,
+                false
+        );
     }
 
     @Override
@@ -58,5 +77,11 @@ public class KilnCategory implements IRecipeCategory<KilnRecipe> {
     public void setRecipe(IRecipeLayoutBuilder builder, KilnRecipe kilnRecipe, IFocusGroup iFocusGroup) {
         builder.addSlot(RecipeIngredientRole.INPUT, 4, 4).addIngredients(kilnRecipe.getIngredients().get(0));
         builder.addSlot(RecipeIngredientRole.OUTPUT, 64, 22).addItemStack(kilnRecipe.getResultItem(null));
+    }
+
+    @Override
+    public void draw(KilnRecipe recipe, IRecipeSlotsView recipeSlotsView, GuiGraphics guiGraphics, double mouseX, double mouseY) {
+        flame.draw(guiGraphics, 5, 24);
+        arrow.draw(guiGraphics, 27, 22);
     }
 }
